b98067fefccfc0452d95186739c73fe7bebfff7f
Unknown <unknown@example.com>
2020-11-04 23:16:03 -0500

    MonedaUseCaseConsume: *extends => MonedaUseCase

--- src/main/java/com/jhw/module/gestion/contabilidad/consume/usecase_def/MonedaUseCaseConsume.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/consume/usecase_def/MonedaUseCaseConsume.java
@@ -2,5 +2,5 @@ package com.jhw.module.gestion.contabilidad.consume.usecase_def;
 
 import com.jhw.module.gestion.contabilidad.core.usecase_def.*;
 
-public interface MonedaUseCaseConsume extends LiquidacionUseCase {
+public interface MonedaUseCaseConsume extends MonedaUseCase {
 }

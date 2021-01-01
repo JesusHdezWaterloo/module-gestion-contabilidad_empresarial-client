72a451c0b4126a44b3f5161de0d9e0318e103910
Unknown <unknown@example.com>
2020-12-31 21:19:53 -0500

    Licence a las clases

--- src/main/java/com/jhw/module/gestion/contabilidad/ui/forma_pago/FormaPagoDetailView.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/ui/forma_pago/FormaPagoDetailView.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jhw.module.gestion.contabilidad.ui.forma_pago;
 
 import com.root101.clean.core.app.services.ExceptionHandler;
-import com.jhw.module.gestion.contabilidad.core.domain.FormaPagoDomain;
+import com.root101.module.gestion.contabilidad.core.domain.FormaPagoDomain;
 import com.jhw.swing.material.components.table.Column;
 import com.jhw.swing.models.input.dialogs.DialogModelInput;
 import com.jhw.swing.models.detail._MaterialPanelDetail;
@@ -10,7 +26,8 @@ import com.jhw.module.gestion.contabilidad.ui.module.ContabilidadSwingModule;
 
 /**
  *
- * @author Jesús Hernández Barrios (jhernandezb96@gmail.com)
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
  */
 public class FormaPagoDetailView extends _MaterialPanelDetail<FormaPagoDomain> {
 
--- src/main/java/com/jhw/module/gestion/contabilidad/ui/module/ContabilidadSwingModule.java
+++ src/main/java/com/jhw/module/gestion/contabilidad/ui/module/ContabilidadSwingModule.java
@@ -1,13 +1,29 @@
+/*
+ * Copyright 2021 Root101 (jhernandezb96@gmail.com, +53-5-426-8660).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Or read it directly from LICENCE.txt file at the root of this project.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jhw.module.gestion.contabilidad.ui.module;
 
 import com.root101.clean.swing.app.AbstractSwingApplication;
 import com.root101.clean.swing.app.DefaultAbstractSwingMainModule;
 import com.root101.clean.swing.app.dashboard.DashBoardSimple;
 import com.root101.clean.swing.app.dashboard.DashboardConstants;
+import com.root101.module.gestion.contabilidad.service.ResourceServiceImplementation;
 import com.jhw.module.gestion.contabilidad.consume.module.ContabilidadConsumeCoreModule;
 import com.jhw.module.gestion.contabilidad.consume.usecase_def.*;
 import com.jhw.module.gestion.contabilidad.service.ResourceServiceClientImplementation;
-import com.jhw.module.gestion.contabilidad.service.ResourceServiceImplementation;
 import com.jhw.module.gestion.contabilidad.ui.cuadre.CuadreDetailView;
 import com.jhw.module.gestion.contabilidad.ui.cuenta.CuentasMainPanel;
 import com.jhw.module.gestion.contabilidad.ui.moneda.MonedaDetailView;
@@ -19,6 +35,11 @@ import com.jhw.swing.material.components.taskpane.CollapseMenu;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 
+/**
+ *
+ * @author Root101 (jhernandezb96@gmail.com, +53-5-426-8660)
+ * @author JesusHdezWaterloo@Github
+ */
 public class ContabilidadSwingModule extends DefaultAbstractSwingMainModule {
 
     private final ContabilidadModuleNavigator navigator = new ContabilidadModuleNavigator();
